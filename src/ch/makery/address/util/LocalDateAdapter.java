0b256993a8a4b252b81eca4a0d7052298e051b91
Oreobites <zettaoreo@gmail.com>
2016-09-18 21:02:22 +0900

    Finished handling menu actions.

---
+++ src/ch/makery/address/util/LocalDateAdapter.java
@@ -0,0 +1,10 @@
+package ch.makery.address.util;
+
+public class LocalDateAdapter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ch/makery/address/view/RootLayoutController.java
@@ -0,0 +1,10 @@
+package ch.makery.address.view;
+
+public class RootLayoutController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
